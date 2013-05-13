efe3c1c1740fa6730f111e8bd8546e7b22da40d9
elbuo8 <yamil.asusta@upr.edu>
2013-05-13 02:08:27 -0400

    Reverted changes

--- index.js
+++ index.js
@@ -34,7 +34,7 @@
             if (response.statusCode === 200) {
               return callback(null, JSON.parse(body).url);
             } else {
-              return callback(body, null);
+              return callback(JSON.parse(body), null);
             }
           }));
         } else {
@@ -70,7 +70,7 @@
           if (response.statusCode === 200) {
             return callback(null, JSON.parse(body).url);
           } else {
-            return callback(body, null);
+            return callback(JSON.parse(body), null);
           }
         });
       });
